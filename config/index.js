00abff7622d163618b11d24a3ab2a1214c00d71c
Angel Hernandez <angel4ernandez@gmail.com>
2024-02-03 02:52:32 -0700

    renamed dist to public_html

--- config/index.js
+++ config/index.js
@@ -45,10 +45,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../public_html/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../public_html'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
